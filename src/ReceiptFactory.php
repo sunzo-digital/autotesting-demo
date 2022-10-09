e7257d48058c776b8acd5ad527b96bb275d76830
sunzo <sunzo.digital@gmail.com>
2022-10-09 19:16:27 +0600

    done

--- src/ReceiptFactory.php
+++ src/ReceiptFactory.php
@@ -8,9 +8,11 @@ class ReceiptFactory
 {
     public function make(string $userName, string $service, int $amount): string
     {
-        $format = "Дорогой \"%s\"!\n"
-            . "Спасибо за заказ: \"%s\"\n"
-            . "С вас было списано: %d тг.\n"
+        $break = str_repeat('-', 30);
+
+        $format = "Дорогой \"%s\"!\n{$break}\n"
+            . "Спасибо за заказ: \"%s\"\n{$break}\n"
+            . "С вас было списано: %d тг.\n{$break}\n"
             . "Ждем вас снова!\n";
 
         return sprintf($format, $userName, $service, $amount);
